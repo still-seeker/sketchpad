322000e19696073a754d3fdbdb1bb412fb4f151f
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-01-12 06:11:22 +0200

    start with adding files to carry the code

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+container.appendChild(grid);
+
+const grid = document.createElement("div");
+grid.classList.add("grid");
+grid.style.width = "16px";
+grid.style.height = "16px";
+grid.style.border = "2px solid white";
+
